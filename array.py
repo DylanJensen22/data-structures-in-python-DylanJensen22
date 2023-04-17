265dffdcde475d6f83b1e0f301e4855526e39433
DylanJensen22 <122431570+DylanJensen22@users.noreply.github.com>
2023-04-16 21:04:52 -0600

    Add files via upload

---
+++ array.py
@@ -0,0 +1,28 @@
+print('Dylan Jensen')
+
+cars = ['Ford', 'Chrysler', 'Dodge', 'Ram', 'Jeep', 'Chevy', 'GMC']
+print(cars)
+
+x = len(cars)
+print(x)
+
+cars.append('Buick')
+print(cars)
+
+print(cars[3])
+
+cars.insert(2, 'Toyota')
+print(cars)
+
+cars.pop(6)
+print(cars)
+
+cars.sort()
+print(cars)
+
+cars.reverse()
+print(cars)
+
+my_array_length = '9'
+array_string = 'The length of my array is'
+print(array_string + ' ' + my_array_length)
